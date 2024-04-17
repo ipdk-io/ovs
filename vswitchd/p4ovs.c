5242b9baf2a1afae8336cfea44b163ceae4c152a
nupuruttarwar <nupur.uttarwar@intel.com>
2024-04-17 13:25:58 -0700

    Correct the qualifier for grpc_addr variablei to fix segmentation fault
    Signed-off-by: nupuruttarwar <nupur.uttarwar@intel.com>

--- vswitchd/p4ovs.c
+++ vswitchd/p4ovs.c
@@ -9,7 +9,7 @@
 #include "openvswitch/p4ovs.h"
 #include "util.h"
 
-const char grpc_addr[32] = "localhost:9559";
+char grpc_addr[32] = "localhost:9559";
 static const char grpc_port[] = ":9559";
 
 void ovs_set_grpc_addr(const char* optarg) {
@@ -17,9 +17,7 @@ void ovs_set_grpc_addr(const char* optarg) {
         ovs_fatal(0, "--grpc_addr is too long (> %lu characters)",
                   sizeof(grpc_addr) - sizeof(grpc_port));
     }
-    // grpc_addr is immutable except for this function.
-    // We must cast away const to initialize it.
-    strncpy((char *)grpc_addr, optarg, sizeof(grpc_addr));
-    strcat((char *)grpc_addr, grpc_port);
+    strncpy(grpc_addr, optarg, sizeof(grpc_addr));
+    strcat(grpc_addr, grpc_port);
 }
 
