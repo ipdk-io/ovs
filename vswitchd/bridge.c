af1e29f744a6c031c5bb9d2f054aad39f121ad7d
Derek Foster <derek.foster@intel.com>
2024-05-21 10:05:50 -0700

    Rename ovsp4rt public functions
    - Renamed the functions that ovs-p4rt publishes for use by OVS to
      conform better to C naming conventions.
    
    - Added .vscode/ and .vslick/ to .gitignore.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- vswitchd/bridge.c
+++ vswitchd/bridge.c
@@ -2228,7 +2228,7 @@ get_tunnel_data(struct netdev *netdev,
      tnl_info->vni = underlay_tnl->vni;
 
      const char* tnl_type = netdev_get_type(netdev);
-     tnl_info->tunnel_type = TunnelTypeStrtoEnum(tnl_type);
+     tnl_info->tunnel_type = ovsp4rt_str_to_tunnel_type(tnl_type);
 
      return 0;
 }
@@ -2301,8 +2301,9 @@ ConfigureP4Target(struct bridge *br, struct port *port,
             tnl_info.bridge_id = br->p4_bridge_id;
             tnl_info.src_port = port->p4_src_port;
 
-            ConfigTunnelTableEntry(tnl_info, insert_entry, grpc_addr);
-            ConfigRxTunnelSrcTableEntry(tnl_info, insert_entry, grpc_addr);
+            ovsp4rt_config_tunnel_entry(tnl_info, insert_entry, grpc_addr);
+            ovsp4rt_config_rx_tunnel_src_entry(tnl_info, insert_entry,
+                                               grpc_addr);
         } else {
             VLOG_ERR("Error retrieving tunnel information, "
                      "skipping programming P4 entry");
@@ -2317,15 +2318,18 @@ ConfigureP4Target(struct bridge *br, struct port *port,
                                                       port->p4_vlan_id,
                                                       port->p4_src_port};
             /* When VLAN tag is configured */
-            ConfigVlanTableEntry(port->p4_vlan_id, insert_entry, grpc_addr);
-            ConfigTunnelSrcPortTableEntry(tnl_src_port_info, insert_entry, grpc_addr);
+            ovsp4rt_config_vlan_entry(port->p4_vlan_id, insert_entry,
+                                      grpc_addr);
+            ovsp4rt_config_tunnel_src_port_entry(tnl_src_port_info,
+                                                 insert_entry, grpc_addr);
         } else {
             /* Wild card VLAN 0 */
             struct src_port_info tnl_src_port_info = {br->p4_bridge_id,
                                                       0,
                                                       port->p4_src_port};
 
-            ConfigTunnelSrcPortTableEntry(tnl_src_port_info, insert_entry, grpc_addr);
+            ovsp4rt_config_tunnel_src_port_entry(tnl_src_port_info,
+                                                 insert_entry, grpc_addr);
         }
         port->is_src_port_configured = insert_entry;
     } else if (!insert_entry || iface->cfg->mac_in_use) {
@@ -2345,8 +2349,10 @@ ConfigureP4Target(struct bridge *br, struct port *port,
                                                       port->p4_vlan_id,
                                                       port->p4_src_port};
 
-            ConfigVlanTableEntry(port->p4_vlan_id, insert_entry, grpc_addr);
-            ConfigSrcPortTableEntry(vsi_src_port_info, insert_entry, grpc_addr);
+            ovsp4rt_config_vlan_entry(port->p4_vlan_id, insert_entry,
+                                      grpc_addr);
+            ovsp4rt_config_src_port_entry(vsi_src_port_info, insert_entry,
+                                          grpc_addr);
         } else if (port->p4_vlan_mode == P4_PORT_VLAN_UNSUPPORTED) {
             /* Do nothing, unsupported vlan mode */
         } else if (port->p4_src_port) {
@@ -2354,7 +2360,8 @@ ConfigureP4Target(struct bridge *br, struct port *port,
                                                       0,
                                                       port->p4_src_port};
 
-            ConfigSrcPortTableEntry(vsi_src_port_info, insert_entry, grpc_addr);
+            ovsp4rt_config_src_port_entry(vsi_src_port_info, insert_entry,
+                                          grpc_addr);
         } else {
             VLOG_DBG("Invalid P4 use case for source port to "
                      "bridge mapping");
