06f9f003b37770b87044c09bafcfa657a2f3ca5d
Derek Foster <derek.foster@intel.com>
2024-07-24 07:23:00 -0700

    Clean things up a bit
    - Remove an unnecessary copyright notice.
    - Wrap a few long lines.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- lib/dpif-netlink-rtnl.c
+++ lib/dpif-netlink-rtnl.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2017 Red Hat, Inc.
- * Copyright (c) 2021-2022 Intel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
